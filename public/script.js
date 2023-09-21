01eb1e9f01d43933a728c4c4a61ee6320fc7c471
KalimGithub <sk22kalim@gmail.com>
2023-09-22 01:51:55 +0530

    added files

--- public/script.js
+++ public/script.js
@@ -1,60 +1,58 @@
-// var socket = io();
+var socket = io();
+let username = "";
 
-// const userNameInput = document.querySelector(".username-input");
-// const joinChatBtn = document.getElementById("join-chat");
-// const userNameForm  = document.querySelector(".form-username");
-// const chatContainer = document.querySelector(".chat-container");
-// const messageInput = document.getElementById("message-input");
-// const sendBtn = document.getElementById("send-btn");
-// const messageContainer = document.querySelector(".message-container")
-
-// let userName = "";
-
-
-// joinChatBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     userName = userNameInput.value;
-//     // checkig if username is empty or not if username is empty then we dont do anything 
-//     // if username is entered by user in input then only change to chat page
-//     if(userName){
-//         userNameForm.style.display = "none";
-//         chatContainer.style.display = "block";
-//         console.log("welcome to chatroom");
-//     }
-// });
+const userNameInput = document.querySelector(".username-input");
+const joinChatBtn = document.getElementById("join-chat");
+const userNameForm  = document.querySelector(".form-username");
+const chatContainer = document.querySelector(".chat-container");
+const messageInput = document.getElementById("message-input");
+const sendBtn = document.getElementById("send-btn");
+const messageContainer = document.querySelector(".message-container")
 
 
+joinChatBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    username = userNameInput.value;
+    // checkig if username is empty or not if username is empty then we dont do anything 
+    // if username is entered by user in input then only change to chat page
+    if(username){
+        userNameForm.style.display = "none";
+        chatContainer.style.display = "block";
+        console.log("welcome to chatroom");
+    }
+});
 
 
-// sendBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     let data = {
-//         id: socket.id,
-//         username: username,
-//         message: messageInput.value,
-//     };
-//     socket.emit("secret message", data);
-//     appendMessage(data, "sent");
-// });
+sendBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let data = {
+        id: socket.id,
+        username: username,
+        message: messageInput.value,
+    };
+    socket.emit("secret message", data);
+    appendMessage(data, "sent");
+});
 
-// socket.on("secret message", (data) => {
-//     if(data.id !==msocket.id){
-//         appendMessage(data, "recieved");
-//     }
-// });
+socket.on("secret message", data => {
+    if(data.id !== socket.id){
+        appendMessage(data, "recieved");
+    }
+});
 
 
-// function appendMessage(data, type){
-//     var msgDiv = document.createElement('div');
-//     msgDiv.innerText = `${data.username} : ${data.message}`;
-//     if(type == "sent"){
-//         msgDiv.setAttribute("class", "message sent");
-//     } else{
-//         msgDiv.setAttribute("class", "message");
-//     }
-//     messageContainer.append(msgDiv);
-//     messageInput.value = "";
-// }
+function appendMessage(data, type){
+    var msgDiv = document.createElement('div');
+    console.log(data.id);
+    msgDiv.innerText = `${data.username} : ${data.message}`;
+    if(type === "sent"){
+        msgDiv.setAttribute("class", "message sent");
+    } else{
+        msgDiv.setAttribute("class", "message");
+    }
+    messageContainer.append(msgDiv);
+    messageInput.value = "";
+}
 
 
 
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// const http = require('http');
-// const server = http.createServer(app);
+const http = require('http');
+const server = http.createServer(app);
 
-// const { Server } = require("socket.io");
+const { Server } = require("socket.io");
 
-// const io = new Server(server);
+const io = new Server(server);
 
-// const PORT = 3232;
+const PORT = 8888;
 
-// io.on("connection", (socket) => {
-//     console.log(socket.id);
-//     socket.on("secret message", (data) => {
-//         io.emit("secret message", data);
-//     })
-// });
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    socket.on('secret message', (data) => {
+        io.emit('secret message', data);
+    })
+});
 
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// server.listen(PORT);
+server.listen(PORT);
